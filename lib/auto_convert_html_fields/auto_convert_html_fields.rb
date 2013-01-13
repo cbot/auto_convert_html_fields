ad562b60aed205a33d14493817d44c9958028670
Kai <derkai@gmail.com>
2013-01-13 16:40:13 +0100

    added a apply_simple_format option

--- lib/auto_convert_html_fields/auto_convert_html_fields.rb
+++ lib/auto_convert_html_fields/auto_convert_html_fields.rb
@@ -9,6 +9,7 @@ module AutoConvertHtmlFields
 	    	def auto_convert_html_fields(options = {})
 				send :before_save, :convert_html_fields
 				send :include, ActionView::Helpers::SanitizeHelper
+				send :include, ActionView::Helpers::TextHelper
 				
 				cattr_accessor :achf_allowed_tags
 				self.achf_allowed_tags = options[:allowed_tags] || {}
@@ -19,7 +20,9 @@ module AutoConvertHtmlFields
 				cattr_accessor :achf_convert_newlines
 				self.achf_convert_newlines = options[:convert_newlines] || false
 				
-				
+				cattr_accessor :achf_apply_simple_format
+				self.achf_apply_simple_format = options[:apply_simple_format] || false
+
 				unless options.include?(:dont_protect_html_fields)
 					content_columns.each do |c|
 						if self.column_names.include?("#{c.name}_html")
@@ -38,6 +41,8 @@ module AutoConvertHtmlFields
 				if self.respond_to?("#{c.name}_html") && !self.send("#{c.name}").nil?
 					if self.class.achf_convert_newlines
 						self.send("#{c.name}_html=", sanitize(BBCodeizer.bbcodeize(self.send("#{c.name}")), options).gsub(/[\s\n\r]+\Z/,"").gsub("\n","<br />"))
+					elsif self.class.achf_apply_simple_format
+						self.send("#{c.name}_html=", simple_format(sanitize(BBCodeizer.bbcodeize(self.send("#{c.name}")), options).gsub(/[\s\n\r]+\Z/,""), {}, {:sanitize => false}))
 					else
 						self.send("#{c.name}_html=", sanitize(BBCodeizer.bbcodeize(self.send("#{c.name}")), options).gsub(/[\s\n\r]+\Z/,""))
 					end
